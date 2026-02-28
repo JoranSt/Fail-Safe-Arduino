38c0924126d2e230b204d7d23d7d80a97954dd07
JoranSt <joran.stavenuiter@hva.nl>
2026-02-28 16:08:18 +0100

    I put an Ultrasonic sensor in the system
    So far i managed to update teh state when a warning oocurs. However i havent managed to put the state back yet when the warning is gone. I also need to think about the time it takes to run the code since i notice its starting to lag

---
+++ Code/Arduino/SystemWithUltrasonicSensor.ino/SystemWithUltrasonicSensor.ino.ino
@@ -0,0 +1,225 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <NewPing.h>
+//look into structs note for self
+//Pin numbers
+int ledPinRed = 5;
+int ledPinGreen = 3;
+int buttonApin = 6;
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+
+
+enum SystemState {
+  ARMING,
+  IDLE,
+  RUNNING,
+  WARNING,
+  ERROR
+};
+SystemState State = ARMING;
+enum Warning : uint8_t { WARN_NONE = 0,
+                         WARN_ULTRASONIC = 1 << 0  // 00000001
+
+};
+uint8_t warnings = WARN_NONE;
+enum Error : uint8_t { ERROR_NONE = 0,
+                       ERROR_ULTRASONIC = 1 << 0  //00000001
+};
+uint8_t errors = ERROR_NONE;
+//Variables for time
+unsigned long lastMillisLed;
+unsigned long lastMillisButton;
+unsigned long timeButtonPressed;
+unsigned long timeSystemOff;
+unsigned long timeSystemOn;
+unsigned long startTime;
+unsigned long readSensor;
+
+
+
+//Variables for blinking
+unsigned int redbrightness;
+unsigned int greenbrightness;
+int redbase = 255;
+int greenbase = 128;
+float fadeFactor = 0;
+
+//Component states variables
+int buttonState = 0;
+int lastButtonState = 1;
+int ledState = HIGH;
+
+//Bools
+bool safeToStart = false;
+bool isFading = false;
+bool isPressed = false;
+bool startingMessageGone = false;
+bool readyToArm = false;
+
+//Variables for LCD
+String startingMessage = "System powered";
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//Variable ultrasonic sensor
+#define MAX_DISTANCE 200
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+double distance;
+double safetyDistance = 30;
+double unsafeDistance = 10;
+
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print(startingMessage);
+  pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinGreen, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);
+  // analogWrite(ledPinRed, redbrightness);
+  // analogWrite(ledPinGreen, greenbrightness);
+  startTime = millis();
+}
+
+void loop() {
+  buttonState = digitalRead(buttonApin);
+  if (!safeToStart && readyToArm) {
+    updateArmingAnimation();
+    handleArmingButton();
+
+  } else if (timePassed(startTime, 5000) && !readyToArm && !safeToStart) {
+    lcd.clear();
+    displayMessage("Ready to arm", 0, 0);
+    readyToArm = true;
+  }
+  if (millis() - readSensor >= 50) {
+    readAllSensors();
+    handleAllSensors();
+    checkWarnings();
+    readSensor = millis();
+  }
+  handleToggleButton();
+  checkIdle();
+  lastButtonState = buttonState;
+}
+void readAllSensors() {
+  distance = readUltraSonic();
+}
+void handleAllSensors() {
+  handleUltrasonic(distance);
+}
+void checkWarnings() {
+  if (warnings == WARN_NONE && State != ARMING) {
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    return;
+  } else if (warnings != WARN_NONE && State == ARMING) {
+    displayMessage("System unsafe", 0, 0);
+  }
+  if (warnings & WARN_ULTRASONIC) {
+    displayMessage("! too close", 0, 1);
+  }
+}
+
+void handleToggleButton() {
+  if (buttonState == HIGH) {
+    isPressed = false;
+  }
+  //turn the system on/off after arming it
+  if (buttonState == LOW && safeToStart == true && timePassed(lastMillisButton, 500) && lastButtonState == HIGH) {
+    toggleSystem();
+    lastMillisButton = millis();
+  }
+}
+void updateArmingAnimation() {
+  if (timePassed(lastMillisLed, 10)) {
+    lastMillisLed = millis();
+    redbrightness = redbase * fadeFactor;
+    greenbrightness = greenbase * fadeFactor;
+    analogWrite(ledPinRed, redbrightness);
+    analogWrite(ledPinGreen, greenbrightness);
+    fadeFactor += (isFading == false) ? 0.01 : -0.01;
+  }
+  fadefactor();
+}
+void handleArmingButton() {
+  // detects pressing button and logs when you started holding it
+  if (buttonState == LOW && buttonState != lastButtonState) {
+    isPressed = true;
+    timeButtonPressed = millis();
+  }
+
+  // arms the system after holding the button for 2 seconds
+  if (isPressed && timePassed(timeButtonPressed, 2000)) {
+    safeToStart = true;
+    toggleSystem();
+  }
+}
+double readUltraSonic() {
+  return sonar.ping_cm();
+}
+void handleUltrasonic(double distance) {
+  if (distance >= unsafeDistance && distance <= safetyDistance) {
+    warnings |= WARN_ULTRASONIC;
+  } else if (unsafeDistance > distance) {
+    errors |= ERROR_ULTRASONIC;
+  } else {
+    warnings &= ~WARN_ULTRASONIC;  // clear warning
+    errors &= ~ERROR_ULTRASONIC;
+  }
+}
+void checkIdle() {
+  //checks if the system has been idle for too long and put it back in the arming state
+  if (State == IDLE && timePassed(timeSystemOff, 5000)) {
+    safeToStart = false;
+    ledState = HIGH;
+    State = ARMING;
+    displayMessage("Check system &", 0, 0);
+    displayMessage("Arm it if safe", 0, 1);
+  }
+}
+
+void toggleSystem() {
+  //switches the led state
+  ledState = !ledState;
+  digitalWrite(ledPinRed, !ledState);
+  digitalWrite(ledPinGreen, ledState);
+
+  handleSystemStateChange(ledState);
+}
+void handleSystemStateChange(int buttonState) {
+  //tracks if the system is off (needed for disarming)
+  if (buttonState == LOW) {
+    State = IDLE;
+    timeSystemOff = millis();
+    lcd.clear();
+    displayMessage("System idle", 0, 0);
+  } else {
+    State = RUNNING;
+    timeSystemOn = millis();
+    lcd.clear();
+    displayMessage("System Running", 0, 0);
+  }
+}
+
+void fadefactor() {
+  //reverses the fadefactor when maximum reached
+  if (fadeFactor >= 1.0) {
+    fadeFactor = 1.0;
+    isFading = true;
+  } else if (fadeFactor <= 0.0) {
+    fadeFactor = 0.0;
+    isFading = false;
+  }
+}
+
+
+void displayMessage(String message, int col, int row) {
+  lcd.setCursor(col, row);
+  lcd.print(message);
+}
+bool timePassed(unsigned long startTime, unsigned long interval) {
+  return millis() - startTime >= interval;
+}
